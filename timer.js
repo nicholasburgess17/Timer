eed86bc67db06fb86b759f9aa31f3e8152e05382
Nicholas Burgess <Nicholas.Burgess13@gmail.comgit config --global user.name nicholas>
2022-10-18 20:14:50 +0000

    accounting for outlier cases (negative numbers, no numbers, NaN)

--- timer.js
+++ timer.js
@@ -1,10 +1,14 @@
 const args = process.argv.slice(2);
 const timer = function() {
   for (let i = 0; i < args.length; i++) {
+    const delay = Number(args[i]);
+    if (delay < 0 || isNaN(delay)) {
+      continue;
+    }
     setTimeout(() => {
       process.stdout.write("\u0007");
-      console.log("beep");
-    }, args[i] * 1000);
+      console.log("beep", delay);
+    }, delay * 1000);
   }
 };
 timer();
